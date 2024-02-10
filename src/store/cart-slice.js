63f9fbf64119ebf19dfd9dd7fdebc27aaeadbf1e
SairaBeg <sairabeg.dev@gmail.com>
2024-02-10 14:41:35 -0500

    Update cart-slice.js

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,1 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    itemsList: [],
+    totalQuantity: 0,
+    showCart: false,
+  },
+  reducers: {
+    addToCart(state, action) {
+      const newItem = action.payload;
+    },
+    removeFromCart() {},
+  },
+});
